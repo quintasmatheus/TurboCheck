cce89c531e4a2dd3db394423c7ced95c7dfdd1c9
matheusiscte <mpclq@iscte-iul.pt>
2024-01-18 23:53:03 +0000

    scrollAnim first approach

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-
-
-window.addEventListener("scroll", function(e) {
-
-    var path = document.querySelector('#line');
-
-// Get length of path... ~577px in this demo
-var pathLength = path.getTotalLength();
-
-// Make very long dashes (the length of the path itself)
-path.style.strokeDasharray = pathLength + ' ' + pathLength;
-
-// Offset the dashes so the it appears hidden entirely
-path.style.strokeDashoffset = pathLength;
- 
-    // What % down is it? 
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-      
-    // Length to offset the dashes
-    var drawLength = pathLength * scrollPercentage;
-    
-    // Draw in reverse
-    path.style.strokeDashoffset = pathLength - drawLength;
-    
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
   });
+});
 
-//console.log(pathLength)
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
